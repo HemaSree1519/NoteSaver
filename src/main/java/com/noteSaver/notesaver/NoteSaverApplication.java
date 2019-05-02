41cdd58d506b6306c5ed73304da326f15f5a02e7
HemaSree1519 <khemasree96@gmail.com>
2019-05-02 14:09:01 +0530

    Initialize a gradle spring boot project

---
+++ src/main/java/com/noteSaver/notesaver/NoteSaverApplication.java
@@ -0,0 +1,13 @@
+package com.noteSaver.notesaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSaverApplication.class, args);
+	}
+
+}
